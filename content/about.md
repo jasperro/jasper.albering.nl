439f91505811d7b1ec0d10625a63e559d2c5009a
jasperro <jasper@albering.nl>
2019-01-30 21:24:04 +0100

    New font, new color scheme, removed night mode, css autoprefixer, image scaling, better rendering and scaling, compatibility with ie, chrome, edge and safari improved and some other changes.

--- content/about.md
+++ content/about.md
@@ -3,6 +3,7 @@ title: "About"
 date: 2019-01-29T12:16:19+01:00
 menu: "main"
 weight: 1
+coverimage: "img/jasperalberinglogo.svg"
 draft: false
 ---
 ### Who I am
--- themes/jasperro/static/js/scripts.js
+++ themes/jasperro/static/js/scripts.js
@@ -1,39 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-  clss = document.getElementsByClassName("hoverheader");
-  for (var i = 0, len = clss.length; i < len; i++) {
-    clss[i].addEventListener('mouseover', function() {
-      document.getElementById("nightmodecontainer").style.display = "block";
-    });
-    clss[i].addEventListener('mouseout', function() {
-      document.getElementById("nightmodecontainer").style.display = "none";
-    });
-  };
-  document.getElementById("nightcheckbox")
-      .addEventListener('change', function() {
-        if (this.checked) {
-          localStorage.nightmode = 1;
-          console.log("Night mode on");
-          clss = document.getElementsByClassName('card');
-          for (var i = 0, len = clss.length; i < len; i++) {
-            clss[i].style.backgroundColor = '#070707';
-            clss[i].style.color = '#ffffff';
-			 };
-        } else {
-          localStorage.removeItem("nightmode");
-          console.log("Night mode off");
-          clss = document.getElementsByClassName('card');
-          for (var i = 0, len = clss.length; i < len; i++) {
-            clss[i].style.backgroundColor = '#fbf7f5';
-            clss[i].style.color = '#101010';
-          };
-        }
-      });
-  if (localStorage.nightmode == 1) {
-    document.getElementById('nightcheckbox').checked = true;
-    clss = document.getElementsByClassName('card');
-    for (var i = 0, len = clss.length; i < len; i++) {
-      clss[i].style.backgroundColor = '#070707';
-      clss[i].style.color = '#ffffff';
-    };
-  };
-});
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-200px";
+    }
+    prevScrollpos = currentScrollPos;
+}
