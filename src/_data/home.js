c0d7d36a6ca15a9f1a07c899ffe36fe78ad96024
jasperro <jasper@albering.nl>
2020-07-02 19:48:13 +0200

    Added netlify contact form

--- src/_data/home.js
+++ src/_data/home.js
@@ -1,20 +1,31 @@
 module.exports = {
     nl: {
-        homeTitle: "Hoi, mijn naam is Jasper.",
-        homeDescription: "Ik ben een echte <strong>techneut</strong>",
-        homeInfo: `
+        title: "Hoi, mijn naam is Jasper.",
+        description: "Ik ben een echte <strong>techneut</strong>",
+        info: `
     <p>Ik ben 16 jaar oud en altijd wel een fan geweest van computers. Er is altijd wel iets interessants geweest aan een mysterieus apparaat dat van alles kan. Deze interesse is met mij gebleven, en nu wil ik er iets mee doen. Het is altijd leuk om iets te bekijken op het eerste gezicht, en dan te kijken wat daar nou achter zit, wat laat het draaien?</p>
     <p>Open source, React, Vue.js, Linux en Home Automation zijn waar ik mij in specialiseer, maar alles in de technologie heeft mijn interesse.</p>
         `,
         projects: "Projecten",
+        contactHeader:
+            "Ik zou het leuk vinden om te chatten, stuur mij een bericht",
+        contactName: "Jouw naam:",
+        contactMail: "Jouw e-mailadres:",
+        contactMessage: "Jouw bericht:",
+        submit: "Verstuur",
     },
     en: {
-        homeTitle: "Hi, my name is Jasper.",
-        homeDescription: "I am a real <strong>techie</strong>",
-        homeInfo: `
+        title: "Hi, my name is Jasper.",
+        description: "I am a real <strong>techie</strong>",
+        info: `
     <p> I am 16 years old and have always loved computers. There has always been something interesting about a mysterious device that can do anything. This interest has remained with me, and now I want to do something with it. It's always nice to see something at first glance, and then see what's behind it, what makes it work? </p>
     <p> Open source, React, Vue.js, Linux and Home Automation are what I specialize in, but everything in technology has my interest. </p>
         `,
         projects: "Projects",
+        contactHeader: "I'd love to chat, send me a message",
+        contactName: "Your name:",
+        contactMail: "Your e-mail address:",
+        contactMessage: "Your message:",
+        submit: "Submit",
     },
 };
