faa2c13eff8c60fbd65d0cace2b464c8b6fb52ad
jasperro <jasper@albering.nl>
2021-01-15 15:48:40 +0100

    Added webpack for quicker tailwind

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,9 @@
-const cssesc = require("cssesc");
-const postcss = require("gulp-postcss");
-const concat = require("gulp-concat");
 const svgSprite = require("gulp-svg-sprite");
 const gulp = require("gulp");
 const spawn = require("child_process").spawn;
-const https = require("https");
 const fs = require("fs");
-let server;
 
 const isProd = process.env.ELEVENTY_ENV === "production";
-
-gulp.task("css", async function () {
-    return gulp
-        .src("./src/assets/styles/main.pcss")
-        .pipe(postcss())
-        .pipe(concat("main.css"))
-        .pipe(gulp.dest("./dist/assets/styles/"));
-});
 
 const svgSpriteConfig = {
     mode: {
@@ -46,15 +33,14 @@ gulp.task("icons", function () {
         .pipe(gulp.dest("./dist/assets/"));
 });
 
-gulp.task("build", gulp.parallel("icons", "css"));
+gulp.task("build", gulp.parallel("icons"));
 
 gulp.task("reload", async function () {
     return browserSync.reload();
 });
 
 gulp.task("eleventy:serve", async function () {
-    if (server) server.kill();
-    server = spawn("eleventy", ["--serve"]);
+    let server = spawn("eleventy", ["--serve"]);
     server.stdout.on("data", function (data) {
         process.stdout.write(data);
     });
@@ -62,10 +48,10 @@ gulp.task("eleventy:serve", async function () {
 });
 
 gulp.task("watch", async function () {
-    gulp.watch(
-        ["./src/assets/styles/**/*.css", "./tailwind.config.js"],
-        gulp.parallel("css")
-    );
+    //    gulp.watch(
+    //        ["./src/assets/styles/**/*.pcss", "./tailwind.config.js"],
+    //        gulp.parallel("css")
+    //    );
     gulp.watch("./src/assets/icons/**/*", gulp.parallel("icons"));
     gulp.watch("./src/**/*.js", async () => {
         return fs.writeFileSync(
@@ -78,47 +64,5 @@ gulp.task("watch", async function () {
 
 gulp.task(
     "dev",
-    gulp.parallel("css", gulp.series("icons", "eleventy:serve"), "watch")
+    gulp.parallel(gulp.series("icons", "eleventy:serve"), "watch")
 );
-
-function renderError(error) {
-    return `
-        /* Error compiling stylesheet */
-        *,
-        *::before,
-        *::after {
-            box-sizing: border-box;
-        }
-        html,
-        body {
-            margin: 0;
-            padding: 0;
-            min-height: 100vh;
-            font-family: monospace;
-            font-size: 1.25rem;
-            line-height:1.5;
-        } 
-        body::before { 
-            content: ''; 
-            background: #000;
-            top: 0;
-            bottom: 0;
-            width: 100%;
-            height: 100%;
-            opacity: 0.7;
-            position: fixed;
-        }
-        body::after { 
-            content: '${cssesc(error)}'; 
-            white-space: pre;
-            display: block;
-            top: 0; 
-            padding: 30px;
-            margin: 50px;
-            width: calc(100% - 100px);
-            color:#721c24;
-            background: #f8d7da;
-            border: solid 2px red;
-            position: fixed;
-        }`;
-}
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+
+const isDev = process.env.NODE_ENV !== "production";
+
+module.exports = {
+    mode: isDev ? "development" : "production",
+    stats: {
+        colors: true,
+        preset: "normal",
+    },
+    performance: { hints: isDev ? false : "warning" },
+    devtool: isDev ? "cheap-module-source-map" : "source-map",
+    entry: [path.resolve(__dirname, "src/assets/styles/index.js")],
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist/assets"),
+        publicPath: "/assets/",
+    },
+    plugins: [
+        new WebpackManifestPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
+    ],
+    optimization: {
+        removeEmptyChunks: true,
+        ...(!isDev && {
+            minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+        }),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.p?css/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                config: true,
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset",
+                generator: {
+                    filename: `images/${
+                        isDev ? "[name][ext]" : "[contenthash][ext]"
+                    }`,
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: `fonts/${
+                        isDev ? "[name][ext]" : "[contenthash][ext]"
+                    }`,
+                },
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            // Helpful alias for importing assets
+            assets: path.resolve(__dirname, "src/assets"),
+        },
+    },
+};
