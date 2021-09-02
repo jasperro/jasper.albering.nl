9b1c65dbd97ed37fca6d3c4bc3d3114bd08a765b
Jasper Albering <jasper@albering.nl>
2021-09-02 18:24:21 +0200

    Made input styles look better.

--- src/_includes/components/Button.js
+++ src/_includes/components/Button.js
@@ -1,7 +1,7 @@
 const iconShortcode = require("../../utils/shortcodes").icon;
 
 module.exports = (text, icon, href = "#") => `
-<a href="${href}" class="py-2 px-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-800 hover:bg-primary-light focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent">${iconShortcode(
+<a href="${href}" class="py-2 px-2 shadow-sm text-sm font-medium rounded-md text-white bg-gray-800 hover:bg-primary-light focus:outline-none focus:ring-2 focus:ring-accent">${iconShortcode(
     icon
 )}&nbsp${text}</a>
 `;
