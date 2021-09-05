64ecf76d9ae4fea8fc44ba334f3a38da9d98e8ff
Jasper Albering <jasper@albering.nl>
2021-09-05 15:41:54 +0200

    Initial stylesheet layout without tailwind.

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
     plugins: [
         require("postcss-import"),
         require("postcss-nested"),
-        require("tailwindcss")("./tailwind.config.js"),
         require("postcss-preset-env")({
             stage: 1,
             features: {
