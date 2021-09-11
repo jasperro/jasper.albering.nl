3092952b4e222ce01360a108c97ca5dd3224a5b4
Jasper Albering <jasper@albering.nl>
2021-09-11 22:48:20 +0200

    Using gulp to generate favicons with new colors.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const svgSprite = require("gulp-svg-sprite");
 const gulp = require("gulp");
 const spawn = require("child_process").spawn;
 const fs = require("fs");
+const favicons = require("gulp-favicons");
 
 const isProd = process.env.ELEVENTY_ENV === "production";
 
@@ -45,6 +46,44 @@ gulp.task("eleventy:serve", async function () {
         process.stdout.write(data);
     });
     return;
+});
+
+gulp.task("favicons", function () {
+    return gulp
+        .src("./src/assets/images/svg/iconlight.svg")
+        .pipe(
+            favicons({
+                appName: "Jasper Albering's Website",
+                appShortName: "Jasper Albering",
+                appDescription: "This is Jasper Albering's Website",
+                developerName: "Jasper Albering",
+                developerURL: "https://jasper.albering.nl/",
+                background: "#008b8b",
+                theme_color: "#008b8b",
+                path: "/assets/images/favicon/",
+                url: "https://jasper.albering.nl/",
+                display: "standalone",
+                orientation: "portrait",
+                scope: "/",
+                start_url: "/",
+                version: 1.0,
+                logging: false,
+                html: "index.html",
+                pipeHTML: true,
+                replace: true,
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: false,
+                    coast: true,
+                    favicons: true,
+                    firefox: false,
+                    windows: true,
+                    yandex: true,
+                },
+            })
+        )
+        .pipe(gulp.dest("./src/assets/images/favicon/", { overwrite: true }));
 });
 
 gulp.task("watch", async function () {
