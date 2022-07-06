415021ed028143c7549b80afa9ed76ec98777ec7
Jasper Albering <jasper@albering.nl>
2022-07-06 10:35:54 +0200

    Update intro text

--- src/_data/home.js
+++ src/_data/home.js
@@ -22,11 +22,11 @@ const defaults = {
 };
 
 const nl = {
-    //description: "Ik leer graag nieuwe dingen",
     info: `
-    Ik ben een techneut. Dat betekent altijd bezig zijn met verschillende projecten, gewoon lekker sleutelen aan van alles. Denk bijvoorbeeld aan het bouwen en "hacken" van kleine apparaatjes, en hun sensordata integreren in Home Assistant, het ontwerpen en bouwen van een applicatie, of gewoon iets anders interessants. Het is altijd leuk om gewoon een beetje rond te snuffelen op GitHub en te kijken wat er allemaal voor slims bedacht is. Technologie is zo interessant omdat het nooit stil staat, er zijn altijd nieuwe en creatieve manieren om echte problemen op te lossen.
-<br><br>
-Momenteel ben ik Computer Science aan het studeren aan de Universiteit Utrecht.
+    <p>Ik werk graag met allerlei soorten technologie.
+      Mijn focus ligt op webontwikkeling,
+      maar daarnaast sleutel ik vooral aan Linux, domotica, netwerken en een beetje elektronica.</p>
+      <p>Momenteel studeer ik Informatica aan de Universiteit Utrecht.</p>
         `,
     workedwith: "Ik heb ervaring met o.a.",
     projects: "Een paar van mijn projecten",
@@ -74,10 +74,10 @@ Momenteel ben ik Computer Science aan het studeren aan de Universiteit Utrecht.
 };
 
 const en = {
-    //description: "I like to learn new things",
-    info: `I'm a techie. That means always working on different projects, just tinkering with everything. Think for example building and "hacking" small devices, and integrating their sensor data into Home Assistant, designing and building applications, or just something else I think is interesting. It's always fun to just look around on GitHub and see what other people have thought up. Technology is so interesting because it never stands still, there are always new and creative ways to solve real problems.
-<br><br>
-I am currently studying Computer Science at Utrecht University.`,
+    info: `<p>I enjoy working with all kinds of technology.
+     My main focus is on web development,
+     but besides that I mostly tinker with Linux, home automation, networking and a little bit of electronics.</p>
+     <p>Currently, I am studying Computer Science at Utrecht University.</p>`,
     workedwith: "I have experience with",
     projects: "Projects",
     about: "About me",
