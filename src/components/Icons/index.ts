15a0b48087832e669fa8c6c8352681f56fee5441
Jasper Albering <jasper@albering.nl>
2022-11-20 08:43:54 +0100

    Make Sprite alias for Icon
    Sprite.Provider seems to be broken for Astro.glob()

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,7 +1,7 @@
 import Icon from "./Icon.astro";
-import Sprite from "./Sprite.astro";
+// import Sprite from "./Sprite.astro";
 
 export const globals = { class: "fill-current" };
 export const defaults = { class: "w-6 h-6" };
 
-export { Icon as default, Icon, Sprite };
+export { Icon as default, Icon, Icon as Sprite };
